e8b0b9e542d00602075409411e586508b4562c1d
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2023-04-08 11:31:48 +1200

    [feat]: Place component.
    Place component created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PlaceComponent } from './components/place/place.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PlaceComponent
   ],
   imports: [
     BrowserModule,
