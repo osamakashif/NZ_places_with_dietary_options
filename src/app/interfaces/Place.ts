530a70fce2bd0f70403527b7db223bb912af9f77
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2023-04-09 12:20:19 +1200

    [feat]: Updated Place.
    Updated Place type.

--- src/app/interfaces/Place.ts
+++ src/app/interfaces/Place.ts
@@ -2,6 +2,8 @@ export interface Place {
     name: string;
     website?: string;
     location?: string;
+    city?: string;
+    cuisine?: string;
     halal?: boolean;
     vegetarian?: boolean;
     vegan?: boolean
