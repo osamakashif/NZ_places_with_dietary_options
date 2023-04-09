f71e775ed8a160edf281ec524173c32186ecc868
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2023-04-09 13:07:58 +1200

    [feat]: Routing added.
    Routing added for application.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlacesListComponent } from './components/places-list/places-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PlacesListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
