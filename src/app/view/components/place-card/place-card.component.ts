01ef731066bec9760a08a6038a5eb54ea2b4f54a
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-07-05 01:57:06 +1200

    [feat]: Title input.
    Title input.

--- src/app/view/components/place-card/place-card.component.ts
+++ src/app/view/components/place-card/place-card.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-place-card',
   standalone: true,
   imports: [],
   templateUrl: './place-card.component.html',
-  styleUrl: './place-card.component.scss'
+  styleUrl: './place-card.component.scss',
 })
 export class PlaceCardComponent {
-
+  @Input() title: string = 'Title';
 }
