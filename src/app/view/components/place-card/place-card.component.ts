a6e09e9d8635d296082cf1292b494b266e31d562
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-07-06 20:31:40 +1200

    [feat]: Dietary option inputs.
    Dietary option inputs.

--- src/app/view/components/place-card/place-card.component.ts
+++ src/app/view/components/place-card/place-card.component.ts
@@ -1,12 +1,19 @@
+import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-place-card',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './place-card.component.html',
   styleUrl: './place-card.component.scss',
 })
 export class PlaceCardComponent {
   @Input() title: string = 'Title';
+  @Input() halal: boolean = false;
+  @Input() halalCertified: boolean = false;
+  @Input() vegetarian: boolean = false;
+  @Input() vegetarianCertified: boolean = false;
+  @Input() vegan: boolean = false;
+  @Input() veganCertified: boolean = false;
 }
