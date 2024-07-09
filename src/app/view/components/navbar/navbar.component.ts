475104c3c492e4a2b98d28220b46bf10ca7363ae
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-07-10 00:35:24 +1200

    [feat]: Custom route checking.
    Custom route checking using Angular location.

--- src/app/view/components/navbar/navbar.component.ts
+++ src/app/view/components/navbar/navbar.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  currentRoute: string = '';
+
+  constructor(private location: Location) {
+    this.refreshRoute();
+  }
+
+  private refreshRoute() {
+    const route: string | undefined = this.location.path().split('/')[1];
+    this.currentRoute = route ? route : '';
+  }
+
+  updateRoute() {
+    setTimeout(() => {
+      this.refreshRoute();
+    }, 0);
+  }
+
+  activeRouteClass(route: string): string {
+    return this.currentRoute === route ? 'active-link' : '';
+  }
+}
