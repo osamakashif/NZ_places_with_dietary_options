0baa3bf521261a043b9b1559176184f46448842f
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-07-14 12:29:13 +1200

    [refactor]: Disclaimer component.
    Disclaimer component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './view/components/header/header.component';
+import { DisclaimerComponent } from './view/components/disclaimer/disclaimer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, DisclaimerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'nz-places-with-dietary-options';
