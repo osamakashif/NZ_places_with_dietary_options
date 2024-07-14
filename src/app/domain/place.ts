2c649422ad10b6b81bbb7e6b6e408ec2b0844d86
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-07-14 13:35:02 +1200

    [feat]: Interfaces.
    Interfaces.

---
+++ src/app/domain/place.ts
@@ -0,0 +1,11 @@
+export interface Place {
+  name: string;
+  website?: string;
+  area?: string;
+  map?: string;
+  city?: string;
+  cuisine?: string;
+  halal?: boolean;
+  vegetarian?: boolean;
+  vegan?: boolean;
+}
